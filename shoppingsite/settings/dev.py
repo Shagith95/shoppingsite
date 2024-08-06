af2f7b38070b2b6758cea501b40be2d470e3cd99
riyak <riyak@qburst.com>
2024-08-07 00:01:19 +0530

    Initial commit

---
+++ shoppingsite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-tu^9r^pabqp=9^xx99aw1d7-4o94$8h$(bn8c(&non990py1o2"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
